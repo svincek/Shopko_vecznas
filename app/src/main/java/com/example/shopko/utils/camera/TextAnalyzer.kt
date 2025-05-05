322bb21a8bace6f12592a5e92cfb025ca4168b7e
simon <simon.vincek@gmail.com>
2025-05-05 11:07:01 +0200

    refractor: sitni code fix

--- app/src/main/java/com/example/shopko/utils/camera/TextAnalyzer.kt
+++ app/src/main/java/com/example/shopko/utils/camera/TextAnalyzer.kt
@@ -1,7 +1,6 @@
 package com.example.shopko.utils.camera
 import android.net.Uri
 import android.util.Log
-import com.example.shopko.data.model.Article
 import com.example.shopko.data.model.ShopkoApp
 import com.example.shopko.data.repository.getArticles
 import com.example.shopko.utils.general.levenshtein
@@ -12,25 +11,7 @@ import java.io.File
 import java.io.IOException
 import java.util.Locale
 
-/**
- * Pokreće prepoznavanje teksta iz slike koristeći Google ML Kit te pokušava prepoznati artikle iz baze podataka
- * uspoređujući prepoznati tekst s poznatim tipovima artikala.
- *
- * @param imageFile Datoteka slike iz koje će se čitati tekst.
- * @param onResult Povratni rezultat u obliku lambda funkcije koja prima listu prepoznatih artikala.
- *
- * Funkcija koristi:
- * - ML Kit za optičko prepoznavanje teksta (OCR)
- * - Levenshteinovu udaljenost za usporedbu sličnosti između prepoznatog teksta i tipova artikala
- *
- * Prepoznati tekst se normalizira (uklanjaju se praznine i pretvara se u velika slova), a zatim se
- * uspoređuje s nazivima tipova artikala iz baze. Ako je Levenshteinova udaljenost manja ili jednaka 3,
- * artikl se smatra prepoznatim.
- *
- * U slučaju greške pri učitavanju slike ili neuspjeha prepoznavanja, vraća se prazna lista.
- */
-
-fun runTextRecognitionOnImage(imageFile: File, onResult: (List<Article>) -> Unit) {
+fun runTextRecognitionOnImage(imageFile: File, onResult: (List<String>) -> Unit) {
     val context = ShopkoApp.getAppContext()
     try {
         val inputImage = InputImage.fromFilePath(context, Uri.fromFile(imageFile))
@@ -43,22 +24,22 @@ fun runTextRecognitionOnImage(imageFile: File, onResult: (List<Article>) -> Unit
                     .map {it.replace("\\s+".toRegex(), "")}
                     .map { it.uppercase(Locale.getDefault()) }
                     .filter { it.isNotBlank() }
+                Log.d("CapturedText", "Recognized text: $textList")
 
-                val products: List<Article> = getArticles()
+                val products: List<String> = getArticles().map { it.type }
 
-                val cleanedArticleList = mutableListOf<Article>()
+                val cleanedTextList = mutableListOf<String>()
 
                 for(text in textList) {
                     for(product in products) {
-                        val distance = levenshtein(text, product.type.replace("\\s+".toRegex(), ""))
+                        val distance = levenshtein(text, product.replace("\\s+".toRegex(), ""))
 
                         if (distance <= 3) {
-                            cleanedArticleList.add(product)
-                            break
+                            cleanedTextList.add(product)
                         }
                     }
                 }
-                onResult(cleanedArticleList.distinct())
+                onResult(cleanedTextList.distinct())
             }
             .addOnFailureListener { e ->
                 Log.e("CapturedText", "Text recognition failed", e)
